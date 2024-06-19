fc37a5a7135b0e59e4660c8cfa45b1ca26b1b235
abdulvokhidjon <162565383+abdulvokhidjon@users.noreply.github.com>
2024-06-19 17:38:19 +0500

    added

--- create-user/src/components/footer/Footer.jsx
+++ create-user/src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="footer">
       <div className="footer-container container">
-        <p>All right Reserved.</p>
+        <p>All right Reserved. <a href="http://t.me/bdulvohid_web">Prepared by: Abdulvohid</a></p>
       </div>
     </div>
   );
